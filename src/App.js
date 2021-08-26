22b3123443747cb1cff5aa357a89481a784d42d8
KrutikGadhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-08-26 23:09:01 +0530

    added login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from "./components/SignIn";
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/'><SignIn /></Route>
+        <Route exact path='/signup'><SignUp /></Route>
       </Switch>
     </Router>
   );
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { Avatar, Button, TextField, Grid, Box, Typography, Container, FormControl, InputAdornment, IconButton, OutlinedInput, InputLabel } from '@material-ui/core'
+import { Avatar, Button, TextField, OutlinedInput, Grid, Box, Typography, Container, FormControl, Select, InputLabel, MenuItem, InputAdornment, IconButton } from '@material-ui/core'
 import { Link } from 'react-router-dom'
-import AccountCircle from '@material-ui/icons/AccountCircle'
+import Face from '@material-ui/icons/FaceOutlined'
 import Visibility from '@material-ui/icons/Visibility'
 import VisibilityOff from '@material-ui/icons/VisibilityOff'
 import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core/styles'
-import bgImg from '../images/svgs/login-bg.svg'
+import bgImg from '../images/svgs/signup-bg.svg'
 
 const theme = createTheme({
   palette: {
@@ -55,7 +55,7 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
     width: '3em',
     height: '3em'
   },
@@ -65,45 +65,61 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    padding: '0.75em'
+    padding: '0.75em',
+    color: '#fff'
   },
 }));
 
-export default function SignIn() {
+export default function SignUp() {
   const classes = useStyles()
+  const [detail, setDetail] = useState({ password: '', confirmPassword: '', name: '', email: '', role: '' });
   const [showPass, setShowPass] = useState(false)
-  const [detail, setDetail] = useState({ password: '', email: '' });
+  const [showCPass, setShowCPass] = useState(false)
 
-  const handleChange = (prop) => (event) => {
-    setDetail({ ...detail, [prop]: event.target.value });
-  };
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+  const handleChange = (prop) => (event) => {
+    setDetail({ ...detail, [prop]: event.target.value });
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Box className={classes.main}>
         <Container component="main" maxWidth="xs">
           <div className={classes.paper}>
             <Avatar className={classes.avatar}>
-              <AccountCircle style={{ fontSize: 30 }} />
+              <Face style={{ fontSize: 30 }} />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              Sign up
             </Typography>
             <Box className={classes.form}>
               <TextField
                 variant="outlined"
                 margin="normal"
+                color="secondary"
+                required
+                fullWidth
+                id="name"
+                label="Full Name"
+                name="name"
+                value={detail.name}
+                onChange={handleChange('name')}
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                // margin="normal"
+                color="secondary"
                 required
                 fullWidth
                 id="email"
                 label="Email Address"
                 name="email"
-                autoComplete="email"
                 value={detail.email}
                 onChange={handleChange('email')}
-                autoFocus
+                autoComplete="email"
               />
               <FormControl style={{ margin: "0.5em 0 0 0" }} fullWidth variant='outlined'>
                 <InputLabel htmlFor="password">Password</InputLabel>
@@ -112,6 +128,7 @@ export default function SignIn() {
                   label="Password"
                   margin="normal"
                   type={showPass ? 'text' : 'password'}
+                  color="secondary"
                   required
                   fullWidth
                   value={detail.password}
@@ -130,29 +147,70 @@ export default function SignIn() {
                   }
                 />
               </FormControl>
+
+              <FormControl style={{ margin: "0.5em 0" }} fullWidth variant='outlined'>
+                <InputLabel htmlFor="Cpassword">Confirm Password</InputLabel>
+                <OutlinedInput
+                  id="Cpassword"
+                  label="Confirm Password"
+                  margin="normal"
+                  type={showCPass ? 'text' : 'password'}
+                  color="secondary"
+                  required
+                  fullWidth
+                  value={detail.confirmPassword}
+                  onChange={handleChange('confirmPassword')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowCPass(!showCPass)}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showCPass ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+              <FormControl fullWidth required variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  color="secondary"
+                  value={detail.role}
+                  onChange={handleChange('role')}
+                  label="Role"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={"Student"}>Student</MenuItem>
+                  <MenuItem value={"Faculty"}>Faculty</MenuItem>
+                  {/* <MenuItem value={"Alimini"}>Alumini</MenuItem> */}
+                  {/* <MenuItem value={"Employer"}>Employer</MenuItem> */}
+                </Select>
+              </FormControl>
               <Button
                 fullWidth
                 variant="contained"
-                color="primary"
+                color="secondary"
                 className={classes.submit}
               >
-                Sign In
+                Sign Up
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link to="/forget">
-                    Forgot password?
-                  </Link>
-                </Grid>
                 <Grid item>
-                  <Link to="/signup">
-                    {"Don't have an account? Sign Up"}
+                  <Link to="/">
+                    {"Already have an account? Sign In"}
                   </Link>
                 </Grid>
               </Grid>
             </Box>
           </div>
-          <Box mt={8}>
+          <Box mt={4}>
             <Copyright />
           </Box>
         </Container>
