4475b72d5ebfaab4ad55b636053c28b1c0736b63
KrutikGadhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-10-31 11:17:04 +0530

    splited feedback for and added feedbackOF

--- src/components/Dashboard/NewFeedback/CreateNewFeedback.js
+++ src/components/Dashboard/NewFeedback/CreateNewFeedback.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
   Dialog,
   Fab,
   FormControl,
+  Grid,
   InputLabel,
   IconButton,
   MenuItem,
@@ -20,6 +21,7 @@ import MuiDialogActions from '@material-ui/core/DialogActions';
 import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
 import CloseIcon from '@material-ui/icons/Close';
 import { DateTimePicker } from '@material-ui/pickers';
+import axios from 'axios'
 
 const styles = makeStyles(theme => ({
   root: {
@@ -37,30 +39,42 @@ const styles = makeStyles(theme => ({
   },
   input: {
     marginBottom: theme.spacing(2)
+  },
+  label: {
+    marginBottom: theme.spacing(2)
   }
 }));
 
-const generateYear = () => {
-  const currentYear = new Date().getFullYear().toString().slice(2, 4)
-  let dep = ['IT', 'CE']
-  let yearList = []
-  dep.forEach(dprt => {
-    for (let i = 18; i <= currentYear; i++) {
-      yearList.push(i + dprt)
-    }
-  })
-  return yearList
-}
+// const generateYear = () => {
+//   const currentYear = new Date().getFullYear().toString().slice(2, 4)
+//   let dep = ['IT', 'CE']
+//   let yearList = []
+//   dep.forEach(dprt => {
+//     for (let i = 18; i <= currentYear; i++) {
+//       yearList.push(i + dprt)
+//     }
+//   })
+//   return yearList
+// }
 
 const Form = props => {
   const { classes } = props;
   const [feedback, setFeedback] = useState({
-    name: '', desc: '', dueFrom: new Date(), dueTo: new Date(), feedbackFor: '', feedbackQue: ''
+    name: '', desc: '', dueFrom: new Date(), dueTo: new Date(), feedbackFor: '', feedbackQue: '', sem: 0, year: new Date().getFullYear(), inst: '', depart: '', feedbackOf: ''
   })
-  // const [name, setName] = useState('');
-  // const [desc, setDesc] = useState('');
-  // const [dueFrom, setDueFrom] = useState(new Date());
-  // const [dueTo, setDueTo] = useState(new Date());
+  const [facultyLst, setFacultyLst] = useState([])
+  const [questionLst, setQuestionLst] = useState([])
+
+  useEffect(() => {
+    axios.get('https://sgp-feedback-system.herokuapp.com/api/faculty').then((res) => {
+      console.log(res)
+      setFacultyLst(res.data)
+    })
+    axios.get('https://sgp-feedback-system.herokuapp.com/api/getfeedbackque').then((res) => {
+      console.log(res)
+      setQuestionLst(res.data.data)
+    })
+  }, [])
 
   const handleChange = (prop) => (event) => {
     if (prop === 'dueTo' || prop === 'dueFrom')
@@ -90,7 +104,7 @@ const Form = props => {
         onChange={handleChange('desc')}
         required
       />
-      <FormControl fullWidth required style={{ margin: "0 0 1em 0" }} variant="outlined" className={classes.formControl}>
+      {/* <FormControl fullWidth required style={{ margin: "0 0 1em 0" }} variant="outlined" className={classes.formControl}>
         <InputLabel id="demo-simple-select-outlined-label">Feedback For</InputLabel>
         <Select
           labelId="demo-simple-select-outlined-label"
@@ -109,23 +123,84 @@ const Form = props => {
             ))
           }
         </Select>
+      </FormControl> */}
+      <Typography className={classes.label}>Feedback For: </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={3}>
+          <TextField
+            label="Sem"
+            variant="outlined"
+            fullWidth
+            // className={classes.input}
+            value={feedback.sem}
+            type='number'
+            onChange={handleChange('sem')}
+            required
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <TextField
+            label="Year"
+            variant="outlined"
+            fullWidth
+            // className={classes.input}
+            value={feedback.year}
+            type='number'
+            onChange={handleChange('year')}
+            required
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <TextField
+            label="Institute"
+            variant="outlined"
+            fullWidth
+            // className={classes.input}
+            value={feedback.inst}
+            onChange={handleChange('inst')}
+            required
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <TextField
+            label="Department"
+            variant="outlined"
+            fullWidth
+            className={classes.input}
+            value={feedback.depart}
+            onChange={handleChange('depart')}
+            required
+          />
+        </Grid>
+      </Grid>
+      <FormControl fullWidth required style={{ margin: "0 0 1em 0" }} variant="outlined" className={classes.formControl}>
+        <InputLabel id="FeedbackOf">Feedback Of faculty</InputLabel>
+        <Select
+          labelId="FeedbackOf"
+          value={feedback.feedbackOf}
+          onChange={handleChange('feedbackOf')}
+          label="Feedback Of"
+          required
+        >
+          {
+            facultyLst.map((item) => (
+              <MenuItem value={item.id} key={item.id}>{item.userName}</MenuItem>
+            ))
+          }
+        </Select>
       </FormControl>
       <FormControl fullWidth required style={{ margin: "0 0 1em 0" }} variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Feedback Question's</InputLabel>
+        <InputLabel id="feedbackQue">Feedback Question's</InputLabel>
         <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
+          labelId="feedbackQue"
           value={feedback.feedbackQue}
           onChange={handleChange('feedbackQue')}
           label="Feedback Question's"
           required
         >
-          <MenuItem selected value="default">
-            <em>Default</em>
-          </MenuItem>
           {
-            ['IT', 'test'].map((item) => (
-              <MenuItem value={item} key={item}>{item}</MenuItem>
+            questionLst.map((item) => (
+              <MenuItem value={item._id} key={item._id}>{item.name}</MenuItem>
             ))
           }
         </Select>
